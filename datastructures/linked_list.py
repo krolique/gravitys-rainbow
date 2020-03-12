04d06afe1b00dfd983c2a363ce37ec6381e6f195
krolique <nick@digitalscripter.com>
2020-03-12 13:10:00 -0400

    wip

--- datastructures/linked_list.py
+++ datastructures/linked_list.py
@@ -4,85 +4,136 @@
 
 """
 
-class Node():
-    """ Node """
+from node import LinkedNode
 
-    def __init__(self, value=None, next=None):
-        """ """
-        self.value = value
-        
-        #:
-        self.next = next
-
-    def __repr__(self):
-        """ """
-
-        if self.next:
-            return f'{self.value}->' + self.next.__repr__()
-        else:
-            return f'{self.value}->{self.next}'
 
 class LinkedList():
-    """ """
-    
-    def __init__(self):
-        """ """
+    """ Linked List """
 
-        self.size = -1
+    def __init__(self):
+        """ Creates a new instance of the LinkedList class"""
+
+        #: defines the number of nodes in the list
+        self.size = 0
+
+        #: defines the reference to the head
         self.head = None
 
     def __repr__(self):
-        """ """
-        
+        """ Returns visual representation of the LinkedList class
+
+        complexity: O(n)
+
+        :rtype: str
+        """
+
         if self.head:
-            return f'{self.head.value}->' + self.head.next.__repr__()
-        else:
-            return f'{self.head.value}->{self.head.next}'
+            return self.head.__repr__()
 
     def size(self):
-        """ """
+        """ Returns the size of the linked list
+
+        complexity: O(1)
+
+        :rtype: int
+        """
 
         return self.size
 
     def add_first(self, value):
-        """ """
+        """ Inserts the specified element at the beginning of this
+        list.
+
+        complexity: O(1)
+
+        :param value: the value to insert
+        :type value: obj
+        """
 
         if self.head is None:
-            self.head = Node(value)
-        pt = Node(value=value)
-        pt.next = self.head
-        self.head = pt
+            self.head = LinkedNode(value)
+        else:
+            pt = LinkedNode(value=value)
+            pt.next = self.head
+            self.head = pt
+
         self.size += 1
 
     def add_last(self, value):
-        """ """
-        
+        """ Appends the specified element to the end of this list.
+
+        complexity: O(n)
+
+        :param value: the value to insert
+        :type value: obj
+        """
+
         pt = self.head
         while pt.next:
             pt = pt.next
 
-        pt.next = Node(value=value)
+        pt.next = LinkedNode(value=value)
         self.size += 1
-        
-    def del(self, target):
-        """ """
 
-        if not self.head:
-            
+    def _ensure_index(self, index):
+        """ Ensures the index value is within the range of the list
+        otherwise raises an IndexError exception
 
+        :para index: the value of the index to evaluate
+        :type index: int
+        """
 
-def del_node(head, target):
-    """ """
+        if index < 0 or index > self.size:
+            raise IndexError('the index is out of range (index < 0 '
+                             f'|| index > {self.size})')
 
-    pt = head
-    if pt.value == target:
-        return pt.next
+    def add_index(self, value, index):
+        """ Inserts the specified element at the specified position in
+        this list.
 
-    while pt.next:
-        if pt.next.value == target:
-            pt.next = pt.next.next
-            return head
-        pt = pt.next
+        complexity: O(n)
 
-    return head
+        :param value: the value to insert
+        :type value: obj
+        :param index: zero based integer value of the values index in
+                      the list
+        :type index: int
+        """
 
+        self._ensure_index(index=index)
+
+        if index == 0:
+            self.add_first(value)
+        pt, cnt = self.head, 1
+        while pt.next:
+            if cnt == index:
+                new_node = LinkedNode(value=value)
+                tmp = pt.next
+                pt.next = new_node
+                new_node.next = tmp
+                self.size += 1
+                return
+            pt = pt.next
+            cnt += 1
+
+    def remove(self, value):
+        """ Removes the first occurrence of the specified element from
+        this list, if it is present.
+
+        complexity: O(n)
+
+        :param value: value of the object to remove
+        :type value: obj
+        """
+
+        pt = self.head
+        if pt.value == value:
+            self.head = pt.next
+
+        while pt.next:
+            if pt.next.value == value:
+                pt.next = pt.next.next
+                return True
+            pt = pt.next
+
+        return False
